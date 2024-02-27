9794de2c1b913a7e4505590073f7bc125e9896a4
marco <marco@crowdsec.net>
2024-02-27 14:49:17 +0100

    wip metrics

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/crowdsecurity/go-cs-bouncer
 go 1.21
 
 require (
-	github.com/crowdsecurity/crowdsec v1.5.5
+	github.com/blackfireio/osinfo v1.0.5
+	github.com/crowdsecurity/crowdsec v1.6.1-0.20240308133748-11e105ae1a80
+	github.com/crowdsecurity/go-cs-lib v0.0.7
 	github.com/prometheus/client_golang v1.17.0
 	github.com/sirupsen/logrus v1.9.3
 	gopkg.in/yaml.v2 v2.4.0
@@ -14,7 +16,6 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/crowdsecurity/go-cs-lib v0.0.5 // indirect
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/go-openapi/analysis v0.21.4 // indirect
 	github.com/go-openapi/errors v0.20.4 // indirect
@@ -26,6 +27,7 @@ require (
 	github.com/go-openapi/swag v0.22.4 // indirect
 	github.com/go-openapi/validate v0.22.1 // indirect
 	github.com/goccy/go-yaml v1.11.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
@@ -34,13 +36,12 @@ require (
 	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.45.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
 	go.mongodb.org/mongo-driver v1.12.1 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
---
+++ metrics.go
@@ -0,0 +1,173 @@
+package csbouncer
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/sirupsen/logrus"
+        "github.com/blackfireio/osinfo"
+
+	"github.com/crowdsecurity/go-cs-lib/version"
+
+	"github.com/crowdsecurity/crowdsec/pkg/apiclient"
+	"github.com/crowdsecurity/crowdsec/pkg/models"
+)
+
+type MetricsUpdater func(*models.RemediationComponentsMetricsItems0)
+
+const (
+	minimumMetricsInterval = 15 * time.Minute
+	defaultMetricsInterval = 30 * time.Minute
+)
+
+func GetMetricsInterval(interval *time.Duration, logger *logrus.Entry) time.Duration {
+	switch {
+	case interval == nil:
+		logger.Tracef("metrics_interval is not set, default to %s", defaultMetricsInterval)
+		return defaultMetricsInterval
+	case *interval == time.Duration(0):
+		logger.Info("metrics_interval is set to 0, disabling metrics")
+		return 0
+	case *interval < minimumMetricsInterval:
+		logger.Warnf("metrics_interval is too low, setting it to %s", minimumMetricsInterval)
+		return minimumMetricsInterval
+	}
+
+	logger.Tracef("metrics_interval set to %s", interval)
+	return *interval
+}
+
+func detectOS() (string, string) {
+	if version.System == "docker" {
+		return "docker", ""
+	}
+
+	osInfo, err := osinfo.GetOSInfo()
+	if err != nil {
+		return version.System, "???"
+	}
+
+	return osInfo.Name, osInfo.Version
+}
+
+type MetricsProvider struct {
+	APIClient *apiclient.ApiClient
+	Interval  time.Duration
+	static    staticMetrics
+	updater   MetricsUpdater
+	logger    *logrus.Entry
+}
+
+type staticMetrics struct {
+	osName            string
+	osVersion         string
+	startupTS         int64
+	featureFlags      []string
+	bouncerType       string
+}
+
+// newStaticMetrics should be called once over the lifetime of the program (more if we support hot-reload)
+func newStaticMetrics(bouncerType string) staticMetrics {
+	osName, osVersion := detectOS()
+
+	return staticMetrics{
+		osName:            osName,
+		osVersion:         osVersion,
+		startupTS:         time.Now().Unix(),
+		featureFlags:      []string{},
+		bouncerType:       bouncerType,
+	}
+}
+
+func NewMetricsProvider(client *apiclient.ApiClient, bouncerType string, interval time.Duration, updater MetricsUpdater, logger *logrus.Entry) (*MetricsProvider, error) {
+	return &MetricsProvider{
+		APIClient: client,
+		Interval: interval,
+		updater: updater,
+		static: newStaticMetrics(bouncerType),
+		logger: logger,
+	}, nil
+}
+
+func (m *MetricsProvider) metricsPayload() *models.AllMetrics {
+	now := time.Now().Unix()
+
+	meta := &models.MetricsMeta{
+		UtcNowTimestamp: now,
+		UtcStartupTimestamp: m.static.startupTS,
+		WindowSizeSeconds: int64(m.Interval.Seconds()),
+	}
+
+	os := &models.OSversion{
+		Name:    m.static.osName,
+		Version: m.static.osVersion,
+	}
+
+	bouncerVersion := version.String()
+
+	base := &models.BaseMetrics{
+		Meta: meta,
+		Os: os,
+		Version: &bouncerVersion,
+		FeatureFlags: m.static.featureFlags,
+	}
+
+	item0 := &models.RemediationComponentsMetricsItems0{
+		BaseMetrics: *base,
+		Type: m.static.bouncerType,
+	}
+
+	if m.updater != nil {
+		m.updater(item0)
+	}
+
+	return &models.AllMetrics{
+		RemediationComponents: []models.RemediationComponentsMetrics{{item0}},
+	}
+}
+
+func (m *MetricsProvider) Run(ctx context.Context) error {
+	if m.Interval == 0 {
+		m.logger.Infof("usage metrics disabled")
+		return nil
+	}
+
+	if m.updater == nil {
+		m.logger.Warningf("no updater provided, metrics will be static")
+	}
+
+	ticker := time.NewTicker(m.Interval)
+
+	m.logger.Tracef("sending usage metrics every %s", m.Interval)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return errors.New("metric provider halted")
+		case <-ticker.C:
+			met := m.metricsPayload()
+
+			ctxTime, cancel := context.WithTimeout(ctx, 10*time.Second)
+			defer cancel()
+
+			_, resp, err := m.APIClient.UsageMetrics.Add(ctxTime, met)
+			switch {
+			case errors.Is(err, context.DeadlineExceeded):
+				m.logger.Warnf("timeout sending metrics")
+				continue
+			case err != nil:
+				m.logger.Warnf("failed to send metrics: %s", err)
+				continue
+			}
+
+			if resp.Response.StatusCode != http.StatusCreated {
+				m.logger.Warnf("failed to send metrics: %s", resp.Response.Status)
+				continue
+			}
+
+			m.logger.Tracef("usage metrics sent")
+		}
+	}
+}
